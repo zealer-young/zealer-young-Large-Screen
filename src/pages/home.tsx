d6233f38e5830ee13f3058a37bf3993ce4d7e2e8
zealer-young <nightwish.ethan@gmail.com>
2024-04-11 17:27:08 +0800

    make grids

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,7 +5,14 @@ import headerBg from "../images/header.png";
 export const Home = () => {
   return (
     <div className="home">
-      <header style={{ backgroundImage: `url(${headerBg})` }}></header>
+      <header style={{ backgroundImage: `url(${headerBg})` }} />
+      <main>
+        <section className="section1"></section>
+        <section className="section2"></section>
+        <section className="section3"></section>
+        <section className="section4"></section>
+        <section className="section5"></section>
+      </main>
     </div>
   );
 };
