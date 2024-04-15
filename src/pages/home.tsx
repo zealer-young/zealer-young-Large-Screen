9848d03e7354360507f961f38038d59d037c9248
zealer-young <nightwish.ethan@gmail.com>
2024-04-15 14:58:49 +0800

    制作 footer

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,4 @@
+
 import React, {useEffect, useRef} from 'react';
 import './home.scss';
 import headerBg from '../images/header.png';
@@ -5,20 +6,24 @@ import {Chart1} from '../components/chart-1';
 import {Chart2} from '../components/chart-2';
 
 export const Home = () => {
+  const year = new Date().getFullYear();
   return (
-    <div className="home">
-      <header style={{backgroundImage: `url(${headerBg})`}}/>
-      <main>
-        <section className="section1">
-          <Chart1/>
-          <Chart2/>
-        </section>
-        <section className="bordered section2">
-        </section>
-        <section className="bordered section3"></section>
-        <section className="bordered section4"></section>
-        <section className="bordered section5"></section>
-      </main>
-    </div>
+      <div className="home">
+        <header style={{backgroundImage: `url(${headerBg})`}}/>
+        <main>
+          <section className="section1">
+            <Chart1/>
+            <Chart2/>
+          </section>
+          <section className="bordered section2">
+          </section>
+          <section className="bordered section3"></section>
+          <section className="bordered section4"></section>
+          <section className="bordered section5"></section>
+        </main>
+        <footer>
+          &copy; Ethan 2024-{year}
+        </footer>
+      </div>
   );
 };
