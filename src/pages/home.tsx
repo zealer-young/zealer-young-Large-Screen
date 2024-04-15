af701247b2ba71efad9428816987c4172b0b4e5b
zealer-young <nightwish.ethan@gmail.com>
2024-04-15 17:56:43 +0800

    charts 初步完成

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -10,6 +10,11 @@ import {Chart6} from '../components/chart-6';
 import {Chart7} from '../components/chart-7';
 import {Chart8} from '../components/chart-8';
 import {Chart9} from '../components/chart-9';
+import {Chart10} from '../components/chart-10';
+import {Chart11} from '../components/chart-11';
+import {Chart12} from '../components/chart-12';
+import {Chart13} from '../components/chart-13';
+import {Chart14} from '../components/chart-14';
 
 export const Home = () => {
   const year = new Date().getFullYear();
@@ -43,17 +48,20 @@ export const Home = () => {
             <div className="bordered row1 案发类型">
               <h2>案发类型统计</h2>
               <div className="charts">
-
+                <Chart10/>
+                <Chart11/>
               </div>
             </div>
             <div className="bordered row2 案发街道">
               <h2>案发街道统计</h2>
               <div className="charts">
-
+                <Chart12/>
+                <Chart13/>
               </div>
             </div>
             <div className="bordered row3 作案手段">
               <h2>作案手段分析</h2>
+              <Chart14/>
             </div>
           </section>
         </main>
